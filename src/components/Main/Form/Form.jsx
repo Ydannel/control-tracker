3779c01b0fae1e17dca9f04c164d415a75d80bfd
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-07-01 11:06:10 -0400

    uptade
    new feature

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -4,7 +4,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { useSpeechContext } from '@speechly/react-client';
 import Snackbar from '../../Snackbar/Snackbar';
+
 import formatDate from '../../../utils/formatDate';
+
 import { ExpenseTrackerContext } from '../../../context/context';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
 import useStyles from './styles';
